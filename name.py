cf15af46699e5f32931fdd5cfa70179ade8db79f
DarkPotatoKing <kyle.rosales97@gmail.com>
2015-12-15 19:54:50 +0800

    Fix TBA bug in name

--- name.py
+++ name.py
@@ -2,9 +2,13 @@ class Name(object):
     """Name object"""
     def __init__(self, name):
         super(Name, self).__init__()
-        name = name.split(',')
-        self.firstname = name[1].strip()
-        self.surname = name[0].strip()
+        if name == 'TBA':
+            self.firstname = name
+            self.surname = name
+        else:
+            name = name.split(',')
+            self.firstname = name[1].strip()
+            self.surname = name[0].strip()
 
     def __repr__(self):
         return ', '.join([self.surname, self.firstname])
@@ -12,4 +16,6 @@ class Name(object):
 
 if __name__ == '__main__':
     n = Name('AGPAOA, JERWIN')
+    m = Name('TBA')
     print n
+    print m
