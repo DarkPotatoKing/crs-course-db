54b07d48268b65ff48b7dd1b550be2781b3c00ca
DarkPotatoKing <kyle.rosales97@gmail.com>
2015-12-15 20:02:34 +0800

    filter by surname

--- database.py
+++ database.py
@@ -19,6 +19,7 @@ class Database(object):
 
         self.courses = list()
         for i in courses:
+            # print i[2]
             self.courses.append(Course(i[0], i[1], i[2], i[3], i[4], i[5]))
 
         # for i in self.courses:
@@ -27,11 +28,17 @@ class Database(object):
     def __repr__(self):
         return '\n\n'.join([str(i) for i in self.courses])
 
+    def filter_by_surname(self, surnames):
+        surnames = [i.upper() for i in surnames]
+        self.courses = filter(lambda x: x.professor.surname in surnames, self.courses)
+        # print self
 
     def sort_by_chance(self):
         self.courses.sort(key = lambda x: float(x.slots) / float(x.demand), reverse = True)
-        print self
+        # print self
 
 if __name__ == '__main__':
     d = Database('sample.txt')
+    d.filter_by_surname(['Perez','De Guzman','Alvares','Agpaoa','Fronda','Mendoza'])
     d.sort_by_chance()
+    print d
