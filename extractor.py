e796afe4b11780678fdb1f7272be51cec74b7305
DarkPotatoKing <kyle.rosales97@gmail.com>
2015-12-15 21:23:09 +0800

    edit extractor

--- extractor.py
+++ extractor.py
@@ -4,11 +4,21 @@ class Extractor(object):
     def extract(cls, filename):
         with open(filename, 'r') as f:
             lines = f.readlines()
-        lines = [i.strip() for i in lines]
+        lines = [i.strip().strip(',').strip() for i in lines]
+        lines = [i.replace(', ', '\t') for i in lines]
         lines = filter(lambda x: x != '', lines)
+
+        s = ''
+        for i in lines:
+            try:
+                int(i.split(',')[0])
+                s += '\n'
+            except:
+                s += ' '
+            s += i
+
         with open(filename, 'w') as f:
-            for i in lines:
-                f.write(str(i) + '\n')
+            f.write(s.lstrip())
 
 if __name__ == '__main__':
-    Extractor.extract('sample.txt')
+    Extractor.extract('sample.csv')
